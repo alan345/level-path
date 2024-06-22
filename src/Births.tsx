9741eb17652d98b798949a576193714b9e09bb74
Alan <alan345@gmail.com>
2024-06-22 14:09:01 -0700

    better css

---
+++ src/Births.tsx
@@ -0,0 +1,21 @@
+import { useContext } from "react";
+import { AppContext } from "./ContextProvider";
+
+const Births = () => {
+  const { births } = useContext(AppContext);
+  if (!births.length) return null;
+  return (
+    <div>
+      <h3>{births.length} Births</h3>
+      <ul className="list-discmarker:text-green list-outside list-disc ml-6">
+        {births.map((birth: any, i) => (
+          <li key={i}>
+            {birth.year}: {birth.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Births;
--- src/SubApp.tsx
+++ src/SubApp.tsx
@@ -1,21 +1,18 @@
-import React, { useContext } from "react";
 import levelpath from "./images/levelpath.png";
-import { AppContext } from "./ContextProvider";
+import GetBirths from "./GetBirths";
+import Births from "./Births";
 
 const SubApp = () => {
-  const { getBirths, births } = useContext(AppContext);
   return (
     <div className="min-h-screen bg-gradient-to-r from-[#EFF8E7] via-[#CEDBC9] to-[#F9FFEF]">
       <header className="text-center flex justify-center items-center">
         <img src={levelpath} alt="logo" className="w-96 object-center" />
       </header>
       <div className="text-center mt-10">
-        <button onClick={getBirths}>getInfo</button>
-        {births.map((birth: any, i) => (
-          <p key={i}>
-            {birth.year}: {birth.text}
-          </p>
-        ))}
+        <GetBirths />
+      </div>
+      <div className="ml-80 mt-10">
+        <Births />
       </div>
     </div>
   );
