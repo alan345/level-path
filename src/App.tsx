20ffc2587d6b61f77db7240621877efbb3fbec71
Alan <alan345@gmail.com>
2024-06-21 22:03:17 -0700

    add css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from "react";
-import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello</h1>
       </header>
+      <div>
+        <button>Click me</button>
+      </div>
     </div>
   );
 }
