677dab411ce41828e26847b3e17ef2cf55dce4d3
Alan <alan345@gmail.com>
2024-06-22 18:21:18 -0700

    better

--- src/components/births/GetBirths.tsx
+++ src/components/births/GetBirths.tsx
@@ -21,12 +21,22 @@ const GetBirths = () => {
     const month = String(today.getMonth() + 1).padStart(2, "0");
     const day = String(today.getDate()).padStart(2, "0");
     const url = `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/${month}/${day}`;
+    // console.log(token);
+    // const bad = `Bearer ${token}`;
+    // const good = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJlODEwMGQyNjQwNjNjYmUzODljZWQwNzg5ZTQwMmVlZiIsImp0aSI6ImJhYTQ1ZTk1MGIxNjU1YTcxZWEyZjQzNWFhMDM1Y2VlZTYxOWM1ZDM2ZTFjMzhlNjFhNzYwZDQ0OTU5OWY3MWI2NDI1NDA2OGYyZGM0MDE0IiwiaWF0IjoxNzE5MDM4Mjg4LjI5OTc1NCwibmJmIjoxNzE5MDM4Mjg4LjI5OTc1NywiZXhwIjozMzI3NTk0NzA4OC4yOTYzNjQsInN1YiI6Ijc1OTI3OTAzIiwiaXNzIjoiaHR0cHM6Ly9tZXRhLndpa2ltZWRpYS5vcmciLCJyYXRlbGltaXQiOnsicmVxdWVzdHNfcGVyX3VuaXQiOjUwMDAsInVuaXQiOiJIT1VSIn0sInNjb3BlcyI6WyJiYXNpYyJdfQ.SWtfjEjK0Uj4UT4eEM8UL_YVQ3df8dy99pCHBR_flagQWZNbsg4rJJ5AZmr5oCBJ45AElQXvF0bPhEezs6K8gBzw6q4WI-3n9Pfsc3hOKc_G2UCk6LggV06P8LqypbWwevnnh28vJuxpT6K3a2vAjuOdQOqhpaXanKvwg-tKyDlmsBWQFjDYRDYsXn7XPHK3Xd9kp_J9BxaAqQmthXKRIr3Q87bKp-\_QShbVEWQrPNA-qQbuxnb8JU0OQsCSHq4C7ExTU3yU7lzqxcTQwJIeONSEfc6zdC9JAEQBfOJfSIk8QSy7vTESSyHOvaEnjYR6NHGtTEjZqSzfeOqsVvoDpAQs3irHjaGJhR7Ib1jF31sO7NPlCmFfC-zNtiI13MZgzn2ih9QTwU8TxY3wnYuc7UOA7Y0VaWFdAupr0lv06EltBkIQH1pJaJRhCY1uoX4zVZM71Drw3prRjUMZFLwDh85p9TcgLlUGHGL8RgfG596IHIr-Hd5cVORxf9C_0BTR_r3Ki71LE1kGH89o96yaK8E8F-zmsRLvrSeMHj09sZ0dYqWicubZWrI44dirBmpW8IxV1vNxPbBs-kwSYDT4kAPGKZ-1kPQmweQBtUUiCwoGB0WUTm3BBe4220W1qPMoSG7PJ3C9k9VxOKypiAwFek1BMVmY0cTZuWd3kTIYAp0`;
+    // console.log(bad === good);
+    // console.log(good);
+    // console.log(bad);
+    // console.log(bad.split("\\").join(""));
 
+    // return;
+    const tokenClean = token.split("\\").join("");
     try {
       const response: AxiosResponse = await axios(url, {
         headers: {
           "Api-User-Agent": "User",
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${tokenClean}`,
+          // Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJlODEwMGQyNjQwNjNjYmUzODljZWQwNzg5ZTQwMmVlZiIsImp0aSI6ImJhYTQ1ZTk1MGIxNjU1YTcxZWEyZjQzNWFhMDM1Y2VlZTYxOWM1ZDM2ZTFjMzhlNjFhNzYwZDQ0OTU5OWY3MWI2NDI1NDA2OGYyZGM0MDE0IiwiaWF0IjoxNzE5MDM4Mjg4LjI5OTc1NCwibmJmIjoxNzE5MDM4Mjg4LjI5OTc1NywiZXhwIjozMzI3NTk0NzA4OC4yOTYzNjQsInN1YiI6Ijc1OTI3OTAzIiwiaXNzIjoiaHR0cHM6Ly9tZXRhLndpa2ltZWRpYS5vcmciLCJyYXRlbGltaXQiOnsicmVxdWVzdHNfcGVyX3VuaXQiOjUwMDAsInVuaXQiOiJIT1VSIn0sInNjb3BlcyI6WyJiYXNpYyJdfQ.SWtfjEjK0Uj4UT4eEM8UL_YVQ3df8dy99pCHBR_flagQWZNbsg4rJJ5AZmr5oCBJ45AElQXvF0bPhEezs6K8gBzw6q4WI-3n9Pfsc3hOKc_G2UCk6LggV06P8LqypbWwevnnh28vJuxpT6K3a2vAjuOdQOqhpaXanKvwg-tKyDlmsBWQFjDYRDYsXn7XPHK3Xd9kp_J9BxaAqQmthXKRIr3Q87bKp-\_QShbVEWQrPNA-qQbuxnb8JU0OQsCSHq4C7ExTU3yU7lzqxcTQwJIeONSEfc6zdC9JAEQBfOJfSIk8QSy7vTESSyHOvaEnjYR6NHGtTEjZqSzfeOqsVvoDpAQs3irHjaGJhR7Ib1jF31sO7NPlCmFfC-zNtiI13MZgzn2ih9QTwU8TxY3wnYuc7UOA7Y0VaWFdAupr0lv06EltBkIQH1pJaJRhCY1uoX4zVZM71Drw3prRjUMZFLwDh85p9TcgLlUGHGL8RgfG596IHIr-Hd5cVORxf9C_0BTR_r3Ki71LE1kGH89o96yaK8E8F-zmsRLvrSeMHj09sZ0dYqWicubZWrI44dirBmpW8IxV1vNxPbBs-kwSYDT4kAPGKZ-1kPQmweQBtUUiCwoGB0WUTm3BBe4220W1qPMoSG7PJ3C9k9VxOKypiAwFek1BMVmY0cTZuWd3kTIYAp0`,
         },
       });
       if (!response?.data) {
