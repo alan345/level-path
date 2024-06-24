6379ea51e9cf3bad5c729a29fbf5002b58358081
Alan <alan345@gmail.com>
2024-06-23 18:37:03 -0700

    better

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -7,24 +7,34 @@ import { AppContext } from "../../context/ContextProvider";
 import BackgroundPage from "./BackgroundPage";
 
 const Main = () => {
-  const { token } = useContext(AppContext);
+  const { token } = useContext(AppContext); // Extract token from the context
+
   return (
     <BackgroundPage>
+      {/* Header with the levelpath logo */}
       <header className="text-center flex justify-center items-center">
         <img src={levelpath} alt="logo" className="w-96 object-center" />
       </header>
 
+      {/* Title */}
       <div className="text-center mt-2">
         <h1>Levelpath frontend take-home task</h1>
       </div>
+
+      {/* InputToken component to enter the token */}
       <div className="text-center mt-8">
         <InputToken />
       </div>
+
+      {/* Conditionally render GetBirths and Births components if token is present */}
       {token && (
         <div>
+          {/* Button to trigger fetching of birth data */}
           <div className="text-center mt-10">
             <GetBirths />
           </div>
+
+          {/* Display the fetched birth data */}
           <div className="mt-10 w-1/2 m-auto">
             <Births />
           </div>
