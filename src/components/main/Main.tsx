b50736a295e60e8ab3a5eb9af4c8a37003974607
Alan <alan345@gmail.com>
2024-07-15 10:33:33 -0700

    add simple context

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -5,6 +5,7 @@ import InputToken from "../token/InputToken";
 import { useContext } from "react";
 import { AppContext } from "../../context/ContextProvider";
 import BackgroundPage from "./BackgroundPage";
+import MainContext from "./MainContext";
 
 const Main = () => {
   const { token } = useContext(AppContext); // Extract token from the context
@@ -15,17 +16,14 @@ const Main = () => {
       <header className="text-center flex justify-center items-center">
         <img src={levelpath} alt="logo" className="w-96 object-center" />
       </header>
-
       {/* Title */}
       <div className="text-center mt-2">
         <h1>Levelpath frontend take-home task</h1>
       </div>
-
       {/* InputToken component to enter the token */}
       <div className="text-center mt-8">
         <InputToken />
       </div>
-
       {/* Conditionally render GetBirths and Births components if token is present */}
       {token && (
         <div>
@@ -33,13 +31,14 @@ const Main = () => {
           <div className="text-center mt-10">
             <GetBirths />
           </div>
-
           {/* Display the fetched birth data */}
           <div className="mt-10 w-1/2 m-auto">
             <Births />
           </div>
         </div>
       )}
+      MainContext
+      <MainContext />
     </BackgroundPage>
   );
 };
